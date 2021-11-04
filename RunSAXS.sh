0958c8c09ae5128db0af097c8fca52429a1524ea
donshen <don.z.shen@gmail.com>
2021-11-04 17:27:26 -0500

    minor change in RunSAXS.sh

--- RunSAXS.sh
+++ RunSAXS.sh
@@ -1,3 +1,5 @@
-    python SAXS_Aniso_taichi.py -b 0.005 -q 1.2 -d test
-    echo "Computing average structure factor..."
-    python SAXS_Avg.py -d test
+#!/bin/bash
+folder=239_460K_8x_2
+python SAXS_Aniso_taichi.py -b 0.005 -q 1.2 -d $folder
+echo "Computing average structure factor..."
+python SAXS_Avg.py -d $folder
